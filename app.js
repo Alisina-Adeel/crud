4eabd391d8b5d5360108f44303d0711b63aea96f
Kamal <kamalshcs@gmail.com>
2026-04-30 05:06:42 +0530

    Implemented delete post functionality using state management

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const container = document.getElementById("postsContainer");
 const loadingMessage = document.getElementById("loadingMessage");
 const errorMessage = document.getElementById("errorMessage");
 
+const state = {
+  posts: []
+};
+
 async function fetchPosts() {
   try {
     const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=10");
     const posts = await res.json();
+
+    state.posts = posts;
     
     loadingMessage.style.display = "none";
 
@@ -21,8 +27,28 @@ function renderPosts(posts) {
     <div class="post">
       <h3>${post.title}</h3>
       <p>${post.body}</p>
+
+      <div class="actions">
+        <button onclick="deletePost(${post.id})" class="danger">
+          Delete Post
+        </button>
+      </div>
     </div>
   `).join("");
 }
 
 fetchPosts();
+
+
+async function deletePost(id) {
+  try {
+    await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+      method: "DELETE"
+    });
+    state.posts = state.posts.filter(post => post.id !== id);
+    renderPosts(state.posts);
+
+  } catch (error) {
+    alert("Error deleting post");
+  }
+}
